9af743a4244ac4b39be57d8f6ebd13b7e84fdbe9
Ilya Bazylchuk <ilya.bazylchuk@gmail.com>
2017-03-28 17:03:42 +0300

    Add more dependences

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,19 +1,21 @@
 require 'mkmf'
 
-DEPENDENCES_UBUNTU = %w{poppler-utils poppler-data libreoffice graphicsmagick tesseract-ocr tesseract-ocr-eng antiword unrtf unzip odt2txt}
-DEPENDENCES_MAC = %w{poppler graphicsmagick tesseract antiword unrtf unzip odt2txt}
+DEPENDENCES_UBUNTU = %w{poppler-utils poppler-data libreoffice graphicsmagick tesseract-ocr tesseract-ocr-eng antiword unrtf unzip odt2txt ghostscript pdftk}
+DEPENDENCES_MAC = %w{poppler graphicsmagick tesseract antiword unrtf unzip odt2txt ghostscript}
 
 puts '****************'
 
 if `uname -a`.include?('Ubuntu')
   dependences = DEPENDENCES_UBUNTU
   command = 'sudo apt-get'
+  list = `apt list --installed`
 else
   dependences = DEPENDENCES_MAC
   command = 'brew'
   list = `brew list`
-  dependences = dependences.reject { |d| list.include?(d) }
 end
+
+dependences = dependences.reject { |d| list.include?(d) }
 
 dependences.each do |tool|
   unless system("which #{ tool }")
--- lib/text_extractor/version.rb
+++ lib/text_extractor/version.rb
@@ -1,3 +1,3 @@
 module TextExtractor
-  VERSION = "0.2.3"
+  VERSION = "0.2.4"
 end

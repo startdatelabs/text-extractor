2250665833eeaf0f3915423aa6a1b66738fb7577
Ilya Bazylchuk <ilya.bazylchuk@gmail.com>
2017-03-29 12:57:18 +0300

    Remove force encode

--- lib/text_extractor/formats/txt.rb
+++ lib/text_extractor/formats/txt.rb
@@ -5,7 +5,7 @@ module TextExtractor
     def extract_text_from_txt(original_file_path)
       text = nil
       if ::File.exist?(original_file_path)
-        text = File.read(original_file_path).force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+        text = File.read(original_file_path)
         text = escape_text(text)
       end
 
--- lib/text_extractor/version.rb
+++ lib/text_extractor/version.rb
@@ -1,3 +1,3 @@
 module TextExtractor
-  VERSION = "0.2.7"
+  VERSION = "0.2.8"
 end
